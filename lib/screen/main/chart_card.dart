383ceef3c44c66541a30369261e5617f32083f1c
gojalifs <fajarsidik1999@gmail.com>
2023-08-13 09:54:23 +0700

    feat:added chart into report page

--- lib/screen/main/chart_card.dart
+++ lib/screen/main/chart_card.dart
@@ -1,5 +1,7 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:satria_optik_admin/provider/report_provider.dart';
 
 class ChartSample extends StatelessWidget {
   const ChartSample({
@@ -12,107 +14,76 @@ class ChartSample extends StatelessWidget {
       child: Container(
         height: 300,
         padding: const EdgeInsets.all(10),
-        child: LineChart(
-          LineChartData(
-            lineTouchData: const LineTouchData(
-              enabled: false,
-            ),
-            gridData: const FlGridData(show: false),
-            titlesData: const FlTitlesData(
-              bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  reservedSize: 32,
-                  interval: 1,
-                  getTitlesWidget: bottomTitleWidgets,
+        child: Consumer<ReportProvider>(
+          builder: (context, report, child) => LineChart(
+            LineChartData(
+              lineTouchData: const LineTouchData(
+                enabled: true,
+              ),
+              gridData: const FlGridData(show: false),
+              titlesData: const FlTitlesData(
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 31,
+                    // interval: 1,
+                    getTitlesWidget: bottomTitleWidgets,
+                  ),
+                ),
+                rightTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+                topTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+                leftTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    getTitlesWidget: leftTitleWidgets,
+                    showTitles: true,
+                    interval: 1,
+                    reservedSize: 40,
+                  ),
                 ),
               ),
-              rightTitles: AxisTitles(
-                sideTitles: SideTitles(showTitles: false),
-              ),
-              topTitles: AxisTitles(
-                sideTitles: SideTitles(showTitles: false),
-              ),
-              leftTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  getTitlesWidget: leftTitleWidgets,
-                  showTitles: true,
-                  interval: 1,
-                  reservedSize: 40,
+              borderData: FlBorderData(
+                show: true,
+                border: Border(
+                  bottom: BorderSide(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .primary
+                          .withOpacity(0.2),
+                      width: 4),
+                  left: const BorderSide(color: Colors.transparent),
+                  right: const BorderSide(color: Colors.transparent),
+                  top: const BorderSide(color: Colors.transparent),
                 ),
               ),
-            ),
-            borderData: FlBorderData(
-              show: true,
-              border: Border(
-                bottom: BorderSide(
-                    color:
-                        Theme.of(context).colorScheme.primary.withOpacity(0.2),
-                    width: 4),
-                left: const BorderSide(color: Colors.transparent),
-                right: const BorderSide(color: Colors.transparent),
-                top: const BorderSide(color: Colors.transparent),
-              ),
-            ),
-            lineBarsData: [
-              LineChartBarData(
-                isCurved: true,
-                curveSmoothness: 0,
-                color: Colors.green.withOpacity(0.5),
-                barWidth: 4,
-                isStrokeCapRound: true,
-                dotData: const FlDotData(show: false),
-                belowBarData: BarAreaData(show: false),
-                spots: const [
-                  FlSpot(1, 1),
-                  FlSpot(3, 4),
-                  FlSpot(5, 1.8),
-                  FlSpot(7, 5),
-                  FlSpot(10, 2),
-                  FlSpot(12, 2.2),
-                  FlSpot(13, 1.8),
-                ],
-              ),
-              LineChartBarData(
-                isCurved: true,
-                color: Colors.pink.withOpacity(0.5),
-                barWidth: 4,
-                isStrokeCapRound: true,
-                dotData: const FlDotData(show: false),
-                belowBarData: BarAreaData(
-                  show: true,
-                  color: Colors.pink.withOpacity(0.2),
+              lineBarsData: [
+                LineChartBarData(
+                  isCurved: true,
+                  color: Colors.pink.withOpacity(0.5),
+                  barWidth: 4,
+                  isStrokeCapRound: true,
+                  dotData: const FlDotData(show: false),
+                  belowBarData: BarAreaData(
+                    show: true,
+                    color: Colors.pink.withOpacity(0.2),
+                  ),
+                  spots: List.generate(31, (index) => index).map((e) {
+                    double qty;
+                    qty = report.reports
+                        .where((element) {
+                          return element.date.day == e;
+                        })
+                        .toList()
+                        .length
+                        .toDouble();
+                    return FlSpot(e.toDouble(), qty);
+                  }).toList(),
                 ),
-                spots: const [
-                  FlSpot(1, 1),
-                  FlSpot(3, 2.8),
-                  FlSpot(7, 1.2),
-                  FlSpot(10, 2.8),
-                  FlSpot(12, 2.6),
-                  FlSpot(13, 3.9),
-                ],
-              ),
-              LineChartBarData(
-                isCurved: true,
-                curveSmoothness: 0,
-                color: Colors.cyan.withOpacity(0.5),
-                barWidth: 2,
-                isStrokeCapRound: true,
-                dotData: const FlDotData(show: true),
-                belowBarData: BarAreaData(show: false),
-                spots: const [
-                  FlSpot(1, 3.8),
-                  FlSpot(3, 1.9),
-                  FlSpot(6, 5),
-                  FlSpot(10, 3.3),
-                  FlSpot(13, 4.5),
-                ],
-              ),
-            ],
-            minX: 0,
-            maxX: 14,
-            maxY: 6,
-            minY: 0,
+              ],
+            ),
           ),
         ),
       ),
@@ -125,49 +96,13 @@ Widget leftTitleWidgets(double value, TitleMeta meta) {
     fontWeight: FontWeight.bold,
     fontSize: 14,
   );
-  String text;
-  switch (value.toInt()) {
-    case 1:
-      text = '1m';
-      break;
-    case 2:
-      text = '2m';
-      break;
-    case 3:
-      text = '3m';
-      break;
-    case 4:
-      text = '5m';
-      break;
-    case 5:
-      text = '6m';
-      break;
-    default:
-      return Container();
-  }
+  String text = '${value.toInt()}';
 
   return Text(text, style: style, textAlign: TextAlign.center);
 }
 
 Widget bottomTitleWidgets(double value, TitleMeta meta) {
-  const style = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 16,
-  );
-  Widget text = const Text('');
-  switch (value.toInt()) {
-    case 2:
-      text = const Text('SEPT', style: style);
-      break;
-    case 7:
-      text = const Text('OCT', style: style);
-      break;
-    case 12:
-      text = const Text('DEC', style: style);
-      break;
-    default:
-      text = const Text('');
-      break;
-  }
+  Widget text = Text('${value.toInt()}');
+
   return text;
 }
--- lib/screen/sales_report/report_monthly_screen.dart
+++ lib/screen/sales_report/report_monthly_screen.dart
@@ -1,51 +1,164 @@
+import 'package:cherry_toast/cherry_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
 import 'package:month_year_picker/month_year_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+
 import 'package:satria_optik_admin/provider/report_provider.dart';
 import 'package:satria_optik_admin/screen/main/chart_card.dart';
 
-class MonthlyReportPage extends StatelessWidget {
+class MonthlyReportPage extends StatefulWidget {
   static String route = '/monthly-report';
   const MonthlyReportPage({super.key});
 
   @override
+  State<MonthlyReportPage> createState() => _MonthlyReportPageState();
+}
+
+class _MonthlyReportPageState extends State<MonthlyReportPage> {
+  bool showFab = true;
+
+  var duration = const Duration(milliseconds: 300);
+  final GlobalKey<SfDataGridState> _key = GlobalKey<SfDataGridState>();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Monthly Report')),
-      floatingActionButton: ElevatedButton(
-        onPressed: () {},
-        child: const Text('Export To Excel'),
+      floatingActionButton: AnimatedSlide(
+        duration: duration,
+        offset: showFab ? Offset.zero : const Offset(0, 2),
+        child: AnimatedOpacity(
+          duration: duration,
+          opacity: showFab ? 1 : 0,
+          child: Consumer<ReportProvider>(
+            builder: (context, value, child) => ElevatedButton(
+              onPressed: () async {
+                // try {
+                await value.exportToExcel(_key);
+                // } catch (e) {
+                //   CherryToast.error(title: Text('$e')).show(context);
+                // }
+              },
+              child: const Text('Export To Excel'),
+            ),
+          ),
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: ListView(
-        children: [
-          Row(
-            children: [
-              const Text('Pick a Month'),
-              Consumer<ReportProvider>(
-                builder: (context, report, child) => TextButton(
-                  onPressed: () async {
-                    var month = await showMonthYearPicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(2023, 1),
-                      lastDate: DateTime.now(),
-                    );
-                    if (month != null) {
-                      report.setMonth = month;
-                    }
-                  },
-                  child: Text(formatMonth(report.month) ?? 'Pick A Date'),
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: (notification) {
+          final ScrollDirection direction = notification.direction;
+          setState(() {
+            if (direction == ScrollDirection.reverse) {
+              showFab = false;
+            } else if (direction == ScrollDirection.forward) {
+              showFab = true;
+            }
+          });
+          return true;
+        },
+        child: ListView(
+          padding: const EdgeInsets.all(10),
+          children: [
+            Row(
+              children: [
+                const Text('Pick a Month'),
+                Consumer<ReportProvider>(
+                  builder: (context, report, child) => TextButton(
+                    onPressed: () async {
+                      var month = await showMonthYearPicker(
+                        context: context,
+                        initialDate: report.month ?? DateTime.now(),
+                        firstDate: DateTime(2023, 1),
+                        lastDate: DateTime.now(),
+                      );
+                      if (month != null) {
+                        report.setMonth = month;
+                        await report.getReport();
+                      }
+                    },
+                    child: Text(formatMonth(report.month) ?? 'Pick A Date'),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: ChartSample(),
-          ),
-        ],
+              ],
+            ),
+            const ChartSample(),
+            const Divider(height: 30),
+            const Center(
+                child: Text(
+              'Detail Data Penjualan',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+            )),
+            SizedBox(
+              child: Consumer<ReportProvider>(
+                  builder: (context, reports, child) {
+                if (reports.dataSource == null) {
+                  return const SizedBox();
+                }
+                return SfDataGrid(
+                  shrinkWrapRows: true,
+                  allowColumnsResizing: true,
+                  verticalScrollPhysics: const NeverScrollableScrollPhysics(),
+                  showHorizontalScrollbar: true,
+                  columnWidthMode: ColumnWidthMode.fitByCellValue,
+                  key: _key,
+                  source: reports.dataSource!,
+                  columns: [
+                    GridColumn(
+                      columnName: 'ID Order',
+                      label: const Center(
+                          child: Text(
+                        'ID Order',
+                        style: TextStyle(fontSize: 20),
+                      )),
+                    ),
+                    GridColumn(
+                      columnName: 'Tanggal',
+                      label: const Center(
+                        child: Text(
+                          'Tanggal',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    GridColumn(
+                      columnName: 'Total',
+                      label: const Center(
+                        child: Text(
+                          'Total',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }
+                  // DataTable(
+                  //   columns: const [
+                  //     DataColumn(label: Text('ID Order')),
+                  //     DataColumn(label: Text('Tanggal')),
+                  //     DataColumn(label: Text('Total')),
+                  //   ],
+                  //   rows: reports.reports
+                  //       .map(
+                  //         (e) => DataRow(
+                  //           cells: [
+                  //             DataCell(Text(e.id)),
+                  //             DataCell(Text(
+                  //               DateFormat('MMMM dd, yyyy').format(e.date),
+                  //             )),
+                  //             DataCell(Text(formatToRupiah('${e.nominal}'))),
+                  //           ],
+                  //         ),
+                  //       )
+                  //       .toList(),
+                  // ),
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
